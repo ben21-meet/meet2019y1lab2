c17233421b23eac6e95e994c489ac5fe71826328
ben21-meet <52948166+ben21-meet@users.noreply.github.com>
2020-01-01 15:36:55 +0200

    Add files via upload

--- Lab#2.py
+++ Lab#2.py
@@ -1,2 +1,14 @@
+list1 = ["Toy","Fruit"]
+list2 = [",Toy"]
+
 def common_numbers(list1,list2):
-	
+	num_list = [1,2]
+	self.list1 = list1
+	self.list2 = list2
+
+for i in list1:
+	print(len(list1),len(list2))
+	print(list1,list2)
+
+
+encoder_caesar = ['a':'d','b':'e','c':'f','d':'g','e':'h','f':'i','g':'j','h':'k','i':'l'.....]
